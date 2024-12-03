ae7ea047507dc24d476aa9a1788ea05c09244318
Min Choi <minc.low@gmail.com>
2024-12-03 22:49:00 +1100

    set if statement for main function

---
+++ script1.py
@@ -0,0 +1,10 @@
+
+from script2 import *
+
+
+def main():
+    print('Main in script 1')
+
+
+if __name__ == '__main__':
+    main()
---
+++ script2.py
@@ -0,0 +1,9 @@
+
+from script1 import *
+
+
+def main():
+    print('Main in script 2')
+
+if __name__ == '__main__':
+    main()
