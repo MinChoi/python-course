1f314dc84251d97d55d150b81eaef8fac819c8f2
Min Choi <minc.low@gmail.com>
2024-12-03 22:06:26 +1100

    test modules

---
+++ test-modules.py
@@ -0,0 +1,18 @@
+# modules
+
+# print(help("modules"))
+
+import math
+print(math.pi)
+
+import math as m
+print(m.pi)
+
+from math import pi
+print(pi)
+
+
+import test_modules_mins_family as test
+print(test.living_city)
+print(test.get_status())
+print(test.get_count())
---
+++ test_modules_mins_family.py
@@ -0,0 +1,7 @@
+living_city = 'melbourne'
+
+def get_count():
+    return 4
+
+def get_status():
+    return 'happy'
